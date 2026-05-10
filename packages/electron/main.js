5c5865a44a192fe0d65f1b3c0c558fc2bb1a4b96
Thomas Legrand <legrand.thomas5@hotmail.fr>
2026-05-10 10:00:58 +0000

    chore: monorepo scaffolding with pnpm workspaces

---
+++ packages/electron/main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require("electron");
+
+const isDev = process.env.NODE_ENV === "development";
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  if (isDev) {
+    win.loadURL("http://localhost:5173");
+    win.webContents.openDevTools();
+  }
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
